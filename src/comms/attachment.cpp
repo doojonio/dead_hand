918c088b51c076e7124da9c3d999df2f6ba74f4a
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2024-12-13 22:17:19 +0500

    refactor(comms): attachment defs

---
+++ src/comms/attachment.cpp
@@ -0,0 +1,23 @@
+
+#include "attachment.h"
+
+namespace comms {
+    Attachment::Attachment(const json& j) :
+        mime(j.at("mime").get<std::string>()),
+        url(util::Url(j.at("url").get<std::string>())) {
+    };
+
+
+    [[nodiscard]] std::string Attachment::ser() {
+        auto j = json{
+            {"mime", mime},
+            {"url", url.get()},
+        };
+
+        return j.dump();
+    };
+
+    [[nodiscard]] std::unique_ptr<Attachment> Attachment::de(const std::string& s) {
+        return std::make_unique<Attachment>(json::parse(s));
+    };
+} // namespace comms
--- src/comms/attachment.h
+++ src/comms/attachment.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <string>
-#include "util/www.h"
-
+#include <memory>
 #include <nlohmann/json.hpp>
+#include <string>
+
+#include "util/www.h"
 
 using json = nlohmann::json;
 
@@ -11,23 +12,8 @@ namespace comms {
     struct Attachment {
         std::string mime;
         util::Url url;
-        Attachment(const json& j) :
-            mime(j.at("mime").get<std::string>()),
-            url(util::Url(j.at("url").get<std::string>())) {
-        };
-
-
-        [[nodiscard]] inline std::string ser() {
-            auto j = json{
-                {"mime", mime},
-                {"url", url.get()},
-            };
-
-            return j.dump();
-        };
-
-        [[nodiscard]] static std::unique_ptr<Attachment> de(const std::string& s) {
-            return std::make_unique<Attachment>(json::parse(s));
-        };
+        Attachment(const json& j);
+        [[nodiscard]] inline std::string ser();
+        [[nodiscard]] static std::unique_ptr<Attachment> de(const std::string& s);
     };
 } // namespace comms
